5cf74cd694b5c07d2b218756c8593da6d6efd0ea
c-bohanon <ChristopherJBohanon@gmail.com>
2024-06-08 10:52:19 -0500

    Solution upload

---
+++ Practice/Python/LeetCode: 30 Days of Pandas/5. Data Aggregation/24. Classes More Than 5 Students.py
@@ -0,0 +1,24 @@
+"""
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+
+ 
+
+Write a solution to find all the classes that have at least five students.
+"""
+
+import pandas as pd
+
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    df = courses.groupby("class")["student"].count().reset_index()
+    df = df[df["student"] >= 5]
+    return df[["class"]]
